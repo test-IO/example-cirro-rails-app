7d696249877a7a58b6eaf4366278379fdb9f4b24
Inderpal Singh <singhinther@gmail.com>
2020-09-02 14:28:35 +0200

    fix datatype

--- db/migrate/20200827131626_add_languages_and_domains_to_user.rb
+++ db/migrate/20200827131626_add_languages_and_domains_to_user.rb
@@ -1,6 +1,6 @@
 class AddLanguagesAndDomainsToUser < ActiveRecord::Migration[6.0]
   def change
-    add_column :users, :languages, :string
-    add_column :users, :domains, :string
+    add_column :users, :languages, :text
+    add_column :users, :domains, :text
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,8 +23,8 @@ ActiveRecord::Schema.define(version: 2020_08_27_131626) do
     t.string "provider"
     t.string "uid"
     t.string "cirro_access_token"
-    t.string "languages"
-    t.string "domains"
+    t.text "languages"
+    t.text "domains"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
