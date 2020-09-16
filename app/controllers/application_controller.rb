4f3bdbdde71ae7b804885dca60a2c9c2ebd59637
Inderpal Singh <singhinther@gmail.com>
2020-09-16 14:29:11 +0200

    on signup, redirect user to my-profile (#18)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    if resource.languages.empty? && resource.domains.empty?
+      edit_user_path(resource)
+    else
+      root_path
+    end
+  end
 end
