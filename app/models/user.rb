c432150bdd353e68372f5f652813351cd5cc3a2b
Inderpal Singh <singhinther@gmail.com>
2020-09-18 13:43:42 +0200

    screenname

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,14 @@ class User < ApplicationRecord
 
   devise :omniauthable, omniauth_providers: %i[cirro]
 
+  validates :screenname, presence: true
+
   after_commit :update_app_worker, on: :update
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
       user.email = auth.info.email
+      user.screenname = auth.info.screenname
       user.password = Devise.friendly_token[0, 20]
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_081555) do
+ActiveRecord::Schema.define(version: 2020_09_18_082840) do
 
   create_table "translation_assignments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "title"
@@ -59,6 +59,7 @@ ActiveRecord::Schema.define(version: 2020_09_16_081555) do
     t.string "cirro_access_token"
     t.text "languages"
     t.text "domains"
+    t.string "screenname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
