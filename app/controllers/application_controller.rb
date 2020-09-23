cc232162784bdf23351995478d689791dd6b4ad6
Inderpal Singh <singhinther@gmail.com>
2020-09-23 14:32:31 +0200

    languages can be nil, blank? is a correct way to check against nil or [] (#21)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
   def after_sign_in_path_for(resource)
-    if resource.languages.empty? && resource.domains.empty?
+    if resource.languages.blank? && resource.domains.blank?
       edit_user_path(resource)
     else
       root_path
